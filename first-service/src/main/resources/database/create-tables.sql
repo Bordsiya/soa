bef5ab0246948211c7b53362bbe4e6b6db1485ba
3ilib0ba <evonniy@mail.ru>
2023-09-25 09:29:21 +0300

    fix a bit soa. in create-tables.sql creating a predata.sql a lot

--- first-service/src/main/resources/database/create-tables.sql
+++ first-service/src/main/resources/database/create-tables.sql
@@ -1,39 +1,39 @@
-create table if not exists soa.coordinates
+create table if not exists coordinates
 (
     id      serial primary key,
     x       double precision not null,
     y       bigint not null
 );
 
-create table if not exists soa.address
+create table if not exists address
 (
     id       serial primary key,
     street   text not null,
     zip_code text
 );
 
-create table if not exists soa.organization_type
+create table if not exists organization_type
 (
     id        serial primary key,
     type_name text not null unique
 );
 
-create table if not exists soa.organization
+create table if not exists organization
 (
     id serial primary key,
     name text not null,
     coordinates_id int not null
-        references soa.coordinates (id),
+        references coordinates (id),
     creation_date timestamp not null default CURRENT_TIMESTAMP,
     annual_turnover double precision not null
         check (annual_turnover > 0),
     type_id int
-        references soa.organization_type (id),
+        references organization_type (id),
     address_id int
-        references soa.address (id)
+        references address (id)
 );
 
-create table if not exists soa.employee
+create table if not exists employee
 (
     id serial primary key,
     first_name text not null,
@@ -42,7 +42,7 @@ create table if not exists soa.employee
     salary int not null
         check ( salary >= 0 ),
     organization_id int not null
-        references soa.organization (id)
+        references organization (id)
 );
 
 
