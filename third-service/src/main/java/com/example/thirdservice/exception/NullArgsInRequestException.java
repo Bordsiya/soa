a86276a247d4eb7d2c7478c099d35b3458958dda
Bordsiya <bordun.2002@mail.ru>
2023-09-25 18:29:16 +0300

    Tomcat with 3 service is working

--- third-service/src/main/java/com/example/thirdservice/exception/NullArgsInRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.example.thirdservice.exception;
-
-import javax.ws.rs.WebApplicationException;
-
-public class NullArgsInRequestException extends WebApplicationException {
-    public NullArgsInRequestException(String arg) {
-        super(String.format("Null args were sent [%s]", arg));
-    }
-}
