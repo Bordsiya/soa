c94b81c09afd67c6b5c9828bb5e46831781e895a
3ilib0ba <evonniy@mail.ru>
2023-09-28 01:16:25 +0300

    front: the start

---
+++ all-frontend/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import Footer from "@/components/Footer.vue";
+</script>
+
+<template>
+
+  <header>
+    <div class="wrapper">
+      <Footer />
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 10vh; /* Занимает 10% высоты */
+  line-height: 1.5;
+  z-index: 1000;
+  display: flex; /* Выравнивание по горизонтали */
+  justify-content: center; /* Выравнивание текста по центру */
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+}
+</style>
