99fe70e488552c431ece00c37f8bc1d6e15e648f
3ilib0ba <evonniy@mail.ru>
2024-01-12 00:11:22 +0300

    lab-4 test ping war ws (mvn clean compile install)

---
+++ second-service-soap/src/main/java/org/example/controller/PingController.java
@@ -0,0 +1,30 @@
+package org.example.controller;
+
+import org.example.catalog.GetPingRequest;
+import org.example.catalog.GetPingResponse;
+import org.example.catalog.PingGetResponseDto;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+
+@Endpoint
+public class PingController {
+    private static final String NAMESPACE_URI = "http://org/example/secondservicesoap/catalog";
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getFlatRequest")
+    @ResponsePayload
+    public GetPingResponse getPing(@RequestPayload GetPingRequest request){
+        Long id = request.getId();
+
+        System.out.println("id = " + id);
+
+        GetPingResponse response = new GetPingResponse();
+        PingGetResponseDto responsePing = new PingGetResponseDto();
+        responsePing.setId(id);
+        response.setPing(responsePing);
+
+        return response;
+    }
+}
