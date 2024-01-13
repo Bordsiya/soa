85ea3d1613a3a4259c368d2f6b01ab6141ccb77d
Bordsiya <bordun.2002@mail.ru>
2024-01-14 00:48:06 +0300

    2 service soap docker

--- second-service-soap/src/main/java/org/example/Main.java
+++ second-service-soap/src/main/java/org/example/Main.java
@@ -3,9 +3,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Main extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
